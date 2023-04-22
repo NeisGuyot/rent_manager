a88ca4b21c8360f47e57070c4309b305959b8657
Guyot Florian <florian.guyot1@epfedu.fr>
2023-04-22 15:32:13 +0200

    Enabled vehicles details

---
+++ src/main/java/com/epf/rentmanager/servlet/VehicleDetailServlet.java
@@ -0,0 +1,60 @@
+package com.epf.rentmanager.servlet;
+
+import com.epf.rentmanager.Exception.ServiceException;
+import com.epf.rentmanager.model.Client;
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.ReservationService;
+import com.epf.rentmanager.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/cars/details")
+public class VehicleDetailServlet extends HttpServlet {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            request.setAttribute("resas",
+                    reservationService.findResaByVehicleId(Integer.parseInt(request.getParameter("id"))));
+            request.setAttribute("vehicule", Integer.parseInt(request.getParameter("id")));
+            request.setAttribute("users", clientService.findAll());
+            request.setAttribute("cars", vehicleService.findAll());
+        } catch (NumberFormatException | ServiceException e) {
+            e.printStackTrace();
+        }
+
+        request.getServletContext().getRequestDispatcher("/WEB-INF/views/vehicles/details.jsp").forward(request,
+                response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
